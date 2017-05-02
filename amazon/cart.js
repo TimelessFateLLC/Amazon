743c4cc4abe1acbd152493a8efffced74b726419
Thon Ly <thonly@heartbank.ceo>
2017-05-01 17:19:10 -0700

    Fix lag bug

--- amazon/cart.js
+++ amazon/cart.js
@@ -14,9 +14,10 @@
             <form class="khj-form" action="http://localhost:9080/amazon" method="get">
                 <span class="a-text-bold">Engrave for $35 more!</span>
                 <input id="khj-asin" type="text" name="asin">
-                <input id="khj-amount" type="text" name="amount">
-                <input id="khj-quantity" type="hidden" name="quantity">
-                <input type="text" name="note" maxlength="32" placeholder="Message">
+                <input id="khj-size" type="text" name="size">
+                <input id="khj-price" type="text" name="price">
+                <input id="khj-quantity" type="number" name="quantity">
+                <input type="text" name="message" maxlength="32" placeholder="Message">
                 <div class='a-button-stack'>
                     <span class='a-button a-spacing-small a-button-primary a-button-icon'>
                         <span class='a-button-inner'>
@@ -29,13 +30,27 @@
             </form>
         `;
 
+        const asin = document.getElementById("khj-asin");
+        const size = document.getElementById("khj-size");
+        const price = document.getElementById("khj-price");
+        const quantity = document.getElementById("khj-quantity");
+
         const update = () => {
-            document.getElementById("khj-asin").value = document.getElementById("ASIN").value;
-            document.getElementById("khj-amount").value = document.getElementById("priceblock_saleprice").textContent.replace('$','');
-            document.getElementById("khj-quantity").value = "1"; //TODO: later?
+
+            const Asin = document.getElementById("ASIN");
+            const Size = document.getElementById("native_dropdown_selected_ring_size");
+            const Price = document.getElementById("priceblock_saleprice");
+            const Quantity = document.getElementById("quantity");
+
+            asin.value = Asin.value;
+            size.value = Size.options[Size.selectedIndex].text;
+            price.value = Price.textContent.replace('$','');
+            quantity.value = Quantity.options[Quantity.selectedIndex].text;
+
         }
-    
+
         let oldLocation = location.href;
+
         setInterval(() => {
             if(location.href != oldLocation) {
                 update();
